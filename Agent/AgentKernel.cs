159a70e36b4801569d2d0f8d1311e2ae82b5f37b
johan suarez <jhoanseb1992@gmail.com>
2024-04-18 19:56:31 +1000

    start the project

---
+++ Agent/AgentKernel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+namespace Agent;
+
+public class AgentSQL
+{
+    private IConfigurationRoot config;
+
+    public AgentSQL()
+    {
+        config = new ConfigurationBuilder().AddUserSecrets<AgentSQL>().Build();
+
+        var apiKey = config["OpenAI:SecretKey"];
+
+        if (string.IsNullOrEmpty(apiKey))
+        {
+            Console.WriteLine("OpenAI API key is missing. Please add it to the user secrets.");
+            return;
+        }
+
+    }
+
+    public void Run()
+    {
+        var builder = Kernel.CreateBuilder();
+        Console.WriteLine("Hello World!");
+    }
+}
---
+++ AgentCLI/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using Agent;
+
+
+
+namespace AgentCLI;
+
+class CLI
+{
+    static void Main(string[] args)
+    {
+        AgentSQL agent = new AgentSQL();
+
+        agent.Run();
+
+        Console.WriteLine("Hello I'm an agent that can help to select the perfect wine for you ocassion. How can I help you today?");
+        string? ask = Console.ReadLine();
+        Console.WriteLine(ask);
+    }
+}
