e58d3fa448a69d79a86cb8d7854de7f47568f6e6
Johan Suarez <jhoanseb1992@gmail.com>
2024-04-24 15:58:35 +1000

    add the sql pluggin

--- Agent/AgentKernel.cs
+++ Agent/AgentKernel.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.ComponentModel;
+using Microsoft.Extensions.Configuration;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.Data.Sqlite;
 
 namespace Agent;
 
@@ -28,3 +30,48 @@ public class AgentSQL
         Console.WriteLine("Hello World!");
     }
 }
+
+public sealed class MathPlugin
+{
+    [KernelFunction, Description("Take the square root of a number")]
+    public static double Sqrt(
+        [Description("The number to take a square root of")] double number1
+    )
+    {
+        return Math.Sqrt(number1);
+    }
+}
+
+
+public sealed class SQLPlugin
+{
+    [KernelFunction, Description("Create a new database")]
+    public static string CreateDatabase([Description("SQL context connection")] SqliteConnection connection)
+    {
+
+        var createTable = connection.CreateCommand();
+        createTable.CommandText = @"
+            CREATE TABLE IF NOT EXISTS wine (
+                id INTEGER PRIMARY KEY,
+                name TEXT NOT NULL,
+                type TEXT NOT NULL,
+                year INTEGER NOT NULL,
+                price REAL NOT NULL,
+                rating REAL NOT NULL
+            );
+        ";
+        try
+        {
+            createTable.ExecuteNonQuery();
+            return "done";
+        }
+        catch (SqliteException e)
+        {
+            Console.WriteLine(e.Message);
+            return e.Message;
+        }
+    }
+
+}
+
+
--- AgentCLI/Program.cs
+++ AgentCLI/Program.cs
@@ -1,7 +1,7 @@
 ﻿
+using Microsoft.Data.Sqlite;
+
 using Agent;
-
-
 
 namespace AgentCLI;
 
@@ -10,11 +10,38 @@ class CLI
     static void Main(string[] args)
     {
         AgentSQL agent = new AgentSQL();
+        CreateDatabase();
 
         agent.Run();
 
         Console.WriteLine("Hello I'm an agent that can help to select the perfect wine for you ocassion. How can I help you today?");
         string? ask = Console.ReadLine();
         Console.WriteLine(ask);
+    }
+
+    static void CreateDatabase()
+    {
+        using var connection = new SqliteConnection("Data Source=wine.db");
+        connection.Open();
+
+        var createTable = connection.CreateCommand();
+        createTable.CommandText = @"
+            CREATE TABLE IF NOT EXISTS wine (
+                id INTEGER PRIMARY KEY,
+                name TEXT NOT NULL,
+                type TEXT NOT NULL,
+                year INTEGER NOT NULL,
+                price REAL NOT NULL,
+                rating REAL NOT NULL
+            );
+        ";
+        try
+        {
+            createTable.ExecuteNonQuery();
+        }
+        catch (SqliteException e)
+        {
+            Console.WriteLine(e.Message);
+        }
     }
 }
